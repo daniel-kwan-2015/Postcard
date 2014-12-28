34e9220981554af8508844923b420c8f1e773578
Pui K Kwan <PuiKwan@Puis-MacBook-Pro.local>
2014-12-28 23:15:27 +0800

    Initial Commit

---
+++ Project3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project3
+//
+//  Created by Pui K Kwan on 28/12/14.
+//  Copyright (c) 2014 Pui K Kwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
